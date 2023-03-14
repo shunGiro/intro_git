e2ea05c9a6b30159901766660d9384f207184e49
shunGiro <shun.jiro.koga.aichi@gmail.com>
2023-03-14 13:24:15 +0900

    test2

---
+++ Chapter03/src/pack/SampleArray.java
@@ -0,0 +1,23 @@
+package pack;
+
+public class SampleArray {
+	public static void main(String[] args) {
+		int[] data1 = new int[2];
+		double[] data2 = new double[3];
+
+		data1[0] = 12;
+		data1[1] = 34;
+
+		data2[0] = 1.2;
+		data2[1] = 3.4;
+		data2[2] = 5.6;
+
+		for(int i = 0; i < data1.length; i++){
+			System.out.println(data1[i]);
+		}
+
+		for(double d : data2){
+			System.out.println(d);
+		}
+	}
+}
---
+++ Chapter03/src/pack/SampleVariable.java
@@ -0,0 +1,17 @@
+package pack;
+
+public class SampleVariable {
+	public static void main(String[] args) {
+		int data1;		// 整数型変数の宣言
+		double data2;	// 実数型変数の宣言
+
+		data1 = 100;
+		data2 = 3.1;
+
+		if(data1 >= 0){
+			System.out.println(data2);
+		}else{
+			System.out.println("Negative value");
+		}
+	}
+}
