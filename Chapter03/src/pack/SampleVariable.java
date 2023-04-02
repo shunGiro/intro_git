7034bc363f882b581826db892a5e6950440080e9
shunGiro <shun.jiro.koga.aichi@gmail.com>
2023-04-02 09:13:05 +0900

    追加 0402

--- Chapter03/src/pack/SampleVariable.java
+++ Chapter03/src/pack/SampleVariable.java
@@ -4,5 +4,13 @@ public class SampleVariable {
 	public static void main(String[] args) {
 		System.out.println("いったん全削除");
 
+		System.out.println(message());
+	}
+
+	/**
+	 * @return
+	 */
+	static String message() {
+		return "いったん全削除後に追加:こんにちわ";
 	}
 }
