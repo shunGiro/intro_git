b25e158464760487879345c33c5915cfbcb70d92
shunGiro <shun.jiro.koga.aichi@gmail.com>
2023-04-02 09:23:00 +0900

    全削除 09:22

--- Chapter03/src/pack/SampleVariable.java
+++ Chapter03/src/pack/SampleVariable.java
@@ -5,41 +5,6 @@ public class SampleVariable {
 		int data1;		// 整数型変数の宣言
 		double data2;	// 実数型変数の宣言
 
-		System.out.println("こんにちは");
-
-		data1 = 100;
-		data2 = 3.1;
-
-		if(data1 >= 0){
-			System.out.println(data2);
-		}else{
-			System.out.println("Negative value");
-		}
-
-		
-		
-		String data3 = "ブランチ作成";	// 実数型変数の宣言
-		System.out.println(data3);
-		
-		
-		String data4 = "ブランチ作成";	// 実数型変数の宣言
-		System.out.println(data4);
-
-
-
-		System.out.println("いったん削除");
-
-
-
-		System.out.println("ブランチ0328での作業");
-		
-		
-		
-		//ブランチ
-
-		
-		String data5 = "ブランチ作成";	// 削除予定
-		System.out.println(data5);
-
+		//全削除
 	}
 }
