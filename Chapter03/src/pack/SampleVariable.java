3e185caa3b66bfaa52c7446148d674ea1dab5321
shunGiro <shun.jiro.koga.aichi@gmail.com>
2023-04-02 19:27:36 +0900

    メソッド func1追加 2023/04/02

--- Chapter03/src/pack/SampleVariable.java
+++ Chapter03/src/pack/SampleVariable.java
@@ -5,6 +5,11 @@ public class SampleVariable {
 		int data1;		// 整数型変数の宣言
 		double data2;	// 実数型変数の宣言
 
-		//全削除
+
+		func1();
+	}
+
+	public static void func1() {
+		System.out.println("func1");
 	}
 }
