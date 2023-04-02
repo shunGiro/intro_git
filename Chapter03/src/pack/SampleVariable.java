c2b0657e1482b22015f67eed646515c61aa55050
shunGiro <shun.jiro.koga.aichi@gmail.com>
2023-04-02 09:14:56 +0900

    Revert "追加 0402"
    This reverts commit 7034bc363f882b581826db892a5e6950440080e9.

--- Chapter03/src/pack/SampleVariable.java
+++ Chapter03/src/pack/SampleVariable.java
@@ -4,13 +4,5 @@ public class SampleVariable {
 	public static void main(String[] args) {
 		System.out.println("いったん全削除");
 
-		System.out.println(message());
-	}
-
-	/**
-	 * @return
-	 */
-	static String message() {
-		return "いったん全削除後に追加:こんにちわ";
 	}
 }
