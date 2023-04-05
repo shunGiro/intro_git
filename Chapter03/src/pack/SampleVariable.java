15a7c36a1b6eb3f55aafc97233df0e0b5415ea97
shunGiro <shun.jiro.koga.aichi@gmail.com>
2023-04-05 23:00:55 +0900

    コミット

--- Chapter03/src/pack/SampleVariable.java
+++ Chapter03/src/pack/SampleVariable.java
@@ -12,19 +12,22 @@ public class SampleVariable {
 		func5();
 	}
 
-	//コメント追加
+	//コメント追加1
 	public static void func1() {
 		System.out.println("func1");
 	}
 
+	//コメント追加2
 	public static void func2() {
 		System.out.println("func2");
 	}
 
+	//コメント追加3
 	public static void func3() {
 		System.out.println("func3");
 	}
 
+	//コメント追加5
 	public static void func5() {
 		System.out.println("func5");
 	}
