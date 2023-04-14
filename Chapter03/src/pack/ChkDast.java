a100a18b5e199190a21245785ae215ee337a888c
USER <USER@DESKTOP-D0UTGT0>
2023-04-14 15:59:09 +0900

    マージの準備

--- Chapter03/src/pack/ChkDast.java
+++ Chapter03/src/pack/ChkDast.java
@@ -7,5 +7,11 @@ public class ChkDast {
 		System.out.println("テスト");
 		System.out.println("こんにちは");
 		System.out.println("追加");
+		
+		
+		//2023/04/14 add
+		System.out.println("TEST");
+		System.out.println("Hellow");
+		System.out.println("ADD");
 	}
 }
