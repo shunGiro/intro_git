ae87c69aa29d0cfdcb09c8267c4395707ce23e1c
shunGiro <shun.jiro.koga.aichi@gmail.com>
2023-04-14 16:08:03 +0900

    0414

--- Chapter03/src/pack/ChkDast.java
+++ Chapter03/src/pack/ChkDast.java
@@ -11,11 +11,11 @@ public class ChkDast {
 		System.out.println("追加");
 
 		//koga shunjiro(2)
-		System.out.println("テスト");
-		System.out.println("こんにちは");
-		System.out.println("追加");
-		
-		
+		System.out.println("テストa");
+		System.out.println("こんにちはb");
+		System.out.println("追加c");
+
+
 		//2023/04/14 add
 		System.out.println("TEST");
 		System.out.println("Hellow");
